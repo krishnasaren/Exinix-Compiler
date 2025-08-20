93ba67becb57ad5dd00efe002751648e142b339c
krishnasaren <112545071+krishnasaren@users.noreply.github.com>
2025-08-20 22:59:01 +0530

    Update lang_design.js

--- examples/lang_design.js
+++ examples/lang_design.js
@@ -1,13 +1,24 @@
 [package --init--]
 base packagename;
 
-[accessModifier - pub (anywhere), pri(inside class), default(onlyinpackage)]
+[accessModifier]
+#class
+  public - normal no annotion --> cls myClass{} --> access anywhere
+  private - $ ----> class $hiddenClass{} --> outer cls can't insider class can be private , accible inside class itself
+  final - const keyword -> class _finalClass{} --> cant be extends
+
+#attrib/method/construct
+  [public -n o annotions]
+  var name : int = 10;
+  fn display(){}
+  construct(a:str,b:int){state.name, }
+  
 
 [var --init--]
 #keyword : <outer_scope >{pub , pri ,default} <inner_scope> {stat, const} <name> :? <type> = <value>
 
-pub static a:? int = 100;
-static b:? str = 100;
+var static a: int = 100;
+var b : str = 100;
 
 
 [comment --init]
@@ -38,6 +49,122 @@ d:? str = (int) a;
 #logic
   &&, ||, !
 
-[
+[array]
+#List
+name: List[str] = ["",""]
+name: List[int] = [1,2,3,5]
+name: List[str,int, model] = [1,"",2.36] //acept multiple type
+
+order,changebale,duplicate allow, can access by index
+method- > add(at last), get(pos),set(),size()
+
+
+#Set
+name: Set(str) = {"s","hrish"}
+name: Set(int) = 
+unorder, unchangable, duplicate not allow
+
+#set
+  part of hashset,treeset,linkedhashset
+#map (dict)
+  part of hashmap,treemap,linkedhashmap
+
+[flow control --init-- ]
+
+if (a > b) {
+  
+}
+
+if (a>b) {
+}else{
+}
+
+if(a > b) {
+}elif(conditon){
+}else{
+}
+
+#ternary 
+  va:? bool = (condition) ? True : False
+
+#switch
+  select(Object){
+    option 1:
+        break;
+    option 2:
+        break;
+    default:
+
+}
+#while 
+while(expression){
+}
+
+#do..while
+do {
+}while();
+
+#for
+for (assign once, condition chekeverytime, acrion everytime ){
+}
+
+#break loop
+  break; 
+#next
+  next;
+
+#lambda_function
+  public c = (args) => :?int {action}
+  void
+  call -> c (args)
+
+#function
+
+  fn display (a:str,b:str) ->int{
+    return 1;
+  
+  }
+
+  var ab = display(a,b);
+
+#classObject
+  imu cls MyClass{
+    var x : int; #public
+    var @name :str; #protected
+    var $age : int; #private
+    
+    __construct__(name : str, age :int){
+          __self__.name = name;
+          this.age = age;
+    }
+    fn $show(a:str, b:str){
+    }
+
+  }
+
+  final myObj = MyClass();
+  Obj cl = MyClass<str, int>()
+
+#classwithTypes
+  cls mClass<T,Q,V>{
+    var p: T;
+    var r : Q;
+    var s:V;
+    __construct__(p:T,q:Q,r:V){
+      this.p = p;
+      thi
+    }
+
+    
+  }
+
+
+  
+
+    
+  
+
+  
+
   
 
